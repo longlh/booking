8ac7fb452ba193603befb38be027b208e9305120
Vuong Ta Quoc <tqv.itvn@gmail.com>
2019-08-16 23:35:06 +0700

    change slider room detail

--- src/resources/pages/room-detail/index.js
+++ src/resources/pages/room-detail/index.js
@@ -1,18 +1,12 @@
 require('js/vendor')
 require('js/main')
-require('lightslider')
-require('jquery-zoom')
+require('ez-plus')
 
 ;(function() {
-  $('#light-slider').lightSlider({
-    gallery: true,
-    item: 1,
-    loop:true,
-    slideMargin: 0,
-    thumbItem: 7,
-    vThumbWidth: 150,
-    onSliderLoad: function (el) {
-      el.find('li').zoom()
-    }
-  });
+  $('#zoom_03').ezPlus({
+    gallery: 'gallery_01',
+    cursor: 'pointer',
+    galleryActiveClass: 'active',
+    imageCrossfade: true
+  })
 })();
